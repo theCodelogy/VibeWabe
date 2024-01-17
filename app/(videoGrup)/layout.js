6ffe8da46cdd9ce258d3d5e0962238c698fc848c
FahimHasan683031 <fahimhasan683031@gmail.com>
2024-01-17 17:54:14 +0600

    setup project foldure structer

---
+++ app/(videoGrup)/layout.js
@@ -0,0 +1,9 @@
+export default function VideoLayout({ children }) {
+    return (
+      <div>
+        <h1>Video Header</h1>
+            {children}
+        <h1>Video Footre</h1>
+      </div>
+    )
+  }
