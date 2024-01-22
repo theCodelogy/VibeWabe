7ce28e841f21bbab296c7a9615a80ee466dc223c
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-01-22 19:33:44 +0600

    make basic music section

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
-
 import FirstTrendingMovies from "@/components/FristLandingPage/FirstTrandingMovies";
 import FristHomeBanner from "@/components/FristLandingPage/FristHomeBanner";
 import FristLandingFeature from "@/components/FristLandingPage/FristLandingFeature";
 import Faq from "@/components/FristLandingPage/QuetionAns/Faq";
 import LandPageFooter from "@/components/shared/footer/landFooter";
 import Navbar from "@/components/shared/navbar/Navbar";
+import FirstMusicTypes from "./../components/FristLandingPage/FirstMusicTypes";
 
 const HomePage = () => {
   return (
     <div className="bg-black">
       <Navbar />
-      <FristHomeBanner/>
-      <FristLandingFeature/>
-      <FirstTrendingMovies/>
+      <FristHomeBanner />
+      <FristLandingFeature />
+      <FirstTrendingMovies />
+      <FirstMusicTypes />
       <Faq />
       <LandPageFooter />
     </div>
