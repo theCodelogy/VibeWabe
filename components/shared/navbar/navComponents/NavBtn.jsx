5430f8eb5ff35638ac6dbfb91b774b79fddba2f8
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-01-23 20:19:28 +0600

    add profile on navbar

--- components/shared/navbar/navComponents/NavBtn.jsx
+++ components/shared/navbar/navComponents/NavBtn.jsx
@@ -1,75 +1,71 @@
-// "use client";
-// import { authContext } from "@/utils/AuthProvider";
-// import Link from "next/link";
-// import { useContext } from "react";
-
-// const NavBtn = () => {
-//   const { user, signout } = useContext(authContext);
-//   console.log(user?.email);
-//   return (
-//     <div className="flex sm:gap-5 lg:gap-8 uppercase">
-//       {user?.email ? (
-//         <>
-//           {" "}
-//           <Link
-//             href={"/login"}
-//             className="bg-[#6D28D9 bg-red-600 font-semibold text-lg drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
-//           >
-//             Login
-//           </Link>
-//           <Link
-//             href={"/signup"}
-//             className="transition-all hover:scale-95 ease-in-out duration-200 shadow-md border-2 border-red-500 font-semibold py-2 px-8 text-white rounded-xl flex items-center justify-center"
-//           >
-//             Sign Up
-//           </Link>
-//         </>
-//       ) : (
-//         <Link href={'/'}>SignOut</Link>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default NavBtn;
-
 "use client";
 import { authContext } from "@/utils/AuthProvider";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const NavBtn = () => {
   const { user, signout } = useContext(authContext);
-  console.log(user?.email);
+  const [showProfile, setShowProfile] = useState(false);
   return (
-    <div className="flex sm:gap-5 lg:gap-8 uppercase">
-      <>
-        {user?.email ? (
-          <Link
-            className="bg-[#6D28D9 bg-red-600 font-semibold text-lg drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
-            href="#"
-            onClick={signout}
+    <>
+      {user?.email ? (
+        <>
+          <div
+            onClick={() => {
+              setShowProfile(!showProfile);
+            }}
+            className="dropdown dropdown-end"
           >
-            SignOut
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src={
+                    user?.photoURL
+                      ? user?.photoURL
+                      : "https://i.ibb.co/vcQZhpc/1921098.png"
+                  }
+                />
+              </div>
+            </label>
+            {showProfile && (
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box space-y-2"
+              >
+                <li>
+                  <p>{user?.displayName}</p>
+                </li>
+                <li>
+                  <p>{user?.email}</p>
+                </li>
+              </ul>
+            )}
+          </div>
+          <button
+            onClick={signout}
+            className="bg-[#6D28D9 bg-red-600 font-semibold text-lg drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+            // className="bg-[#F29F05] drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <div className="flex sm:gap-5 lg:gap-8 uppercase">
+          <Link
+            href={"/login"}
+            className="bg-[#6D28D9 bg-red-600 font-semibold text-lg drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+          >
+            Login
           </Link>
-        ) : (
-          <>
-            <Link
-              href={"/login"}
-              className="bg-[#6D28D9 bg-red-600 font-semibold text-lg drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
-            >
-              Login
-            </Link>
-            <Link
-              href={"/signup"}
-              className="transition-all hover:scale-95 ease-in-out duration-200 shadow-md border-2 border-red-500 font-semibold py-2 px-8 text-white rounded-xl flex items-center justify-center"
-            >
-              Sign Up
-            </Link>
-          </>
-        )}
-      </>
-    </div>
+          <Link
+            href={"/signUp"}
+            className="transition-all hover:scale-95 ease-in-out duration-200 shadow-md border-2 border-red-500 font-semibold py-2 px-8 text-white rounded-xl flex items-center justify-center"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 
