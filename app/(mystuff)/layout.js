618841619b58d13c20e440d34716fbb7a333c3a3
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-01-25 21:53:12 +0600

    <layout change>: add layout on myStuff page

---
+++ app/(mystuff)/layout.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/shared/footer/footer";
+import Navbar from "@/components/shared/navbar/Navbar";
+
+export default function MusicLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+}
