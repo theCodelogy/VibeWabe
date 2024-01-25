61b580313bc6fa3b0debb0dd879fcea0b26c0994
mkrafiyah <rafiyah.mahbub@gmail.com>
2024-01-25 18:50:49 +0400

    new stuff added

---
+++ app/(mystuff)/mystuff/page.js
@@ -0,0 +1,26 @@
+
+// import StuffBanner from "@/components/ui/CategoryGroupComponents/MyStuffPage/StuffBanner";
+// import Watched from "@/components/ui/CategoryGroupComponents/MyStuffPage/Watched";
+
+import StuffBanner from "@/components/ui/categoryGroupComponents/MyStuffPage/StuffBanner";
+import Watched from "@/components/ui/categoryGroupComponents/MyStuffPage/Watched";
+
+
+
+const page = () => {
+    return (
+        <div>
+            <div>
+          <StuffBanner></StuffBanner>
+
+          <div>
+            <Watched></Watched>
+          </div>
+           
+           
+            </div>
+        </div>
+    );
+};
+
+export default page;
