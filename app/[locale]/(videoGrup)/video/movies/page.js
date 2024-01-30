282e19af2ea554903d31931d0c303d3d05148ca7
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-01-30 23:54:06 +0600

    test

--- app/[locale]/(videoGrup)/video/movies/page.js
+++ app/[locale]/(videoGrup)/video/movies/page.js
@@ -4,18 +4,19 @@ import Filter from "@/components/ui/VideoGrupComponents/movieCategory/Filter";
 import MovieTrandingCard from "@/components/ui/VideoGrupComponents/movieCategory/MovieTrandingCard";
 import MovieUpComingCard from "@/components/ui/VideoGrupComponents/movieCategory/MovieUpComingCard";
 
-import MovieTrailerOne from '@/components/ui/VideoGrupComponents/movieCategory/MovieTrailerOne';
+import MovieTrailerOne from "@/components/ui/VideoGrupComponents/movieCategory/MovieTrailerOne";
+import ComedyMovies from "@/components/ui/VideoGrupComponents/movieCategory/ComedyMovies";
 const Page = () => {
-    return (
-        <div>
-            <MovieBanner />
-            <Filter/>
-            <MovieTrandingCard/>
-            <MovieTrailerOne/>
-            <MovieUpComingCard/>
-          
-        </div>
-    );
+  return (
+    <div>
+      <MovieBanner />
+      <Filter />
+      <MovieTrandingCard />
+      <MovieTrailerOne />
+      <MovieUpComingCard />
+      {/* <ComedyMovies /> */}
+    </div>
+  );
 };
 
 export default Page;
--- components/ui/VideoGrupComponents/movieCategory/MovieUpComingCard.js
+++ components/ui/VideoGrupComponents/movieCategory/MovieUpComingCard.js
@@ -1,173 +1,24 @@
-// 'use client'
-// import Image from 'next/image';
-// import React from 'react';
-// import Slider from "react-slick";
-// import slide1 from '@/assets/Recomended-movie/d3e6plk-14712041-47d4-43f7-9806-8aa132e62941.jpg'
-// import slide2 from '@/assets/Recomended-movie/s-l1200.webp'
-// import slide3 from '@/assets/Recomended-movie/the-black-demon.jpg'
-// import slide4 from '@/assets/Recomended-movie/65.jpg'
-// import slide5 from '@/assets/Recomended-movie/the-tank.jpeg'
-
-// function SampleNextArrow(props) {
-//     const { className, style, onClick } = props;
-//     return (
-//       <div
-//         className={className}
-//         style={{ ...style, display: "block", color:'red', }}
-//         onClick={onClick}
-        
-//       />
-      
-//     );
-//   }
-
-// const MovieUpComingCard = () => {
-//     const settings = {
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         slidesToShow: 4,
-//         slidesToScroll: 4,
-//         nextArrow: <SampleNextArrow />,
-//         responsive: [
-//             {
-//               breakpoint: 1024,
-//               settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 infinite: true,
-//                 dots: true
-//               }
-//             },
-//             {
-//               breakpoint: 600,
-//               settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 2,
-//                 initialSlide: 2
-//               }
-//             },
-//             {
-//               breakpoint: 480,
-//               settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//               }
-//             }
-//           ]
-//     };
-//     return (
-//         <div className='max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5 my-10'>
-//               <h1 className='text-2xl my-5 text-white font-bold'>Upcoming Movie </h1>
-//             <div>
-//                 <Slider {...settings}>
-// {/* slide One */}
-//                 <div>
-//         <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-//           <Image src={slide1} loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          
-//         </a>
-
-//         <div className="flex flex-col">
-//           <span className="text-gray-200">JHON WICK</span>
-//           <a href="#" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Business Causual</a>
-//         </div>
-//       </div>
-
-// {/* end slide one */}
-
-
-// {/* slide two */}
-// <div>
-//         <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-//           <Image src={slide2} loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          
-//         </a>
-
-//         <div className="flex flex-col">
-//           <span className="text-gray-200">JHON WICK</span>
-//           <a href="#" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Business Causual</a>
-//         </div>
-//       </div>
-//     {/* end slide two */}
-//                     {/* slide three */}
-//                     <div>
-//         <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-//           <Image src={slide3} loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          
-//         </a>
-
-//         <div className="flex flex-col">
-//           <span className="text-gray-200">JHON WICK</span>
-//           <a href="#" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Business Causual</a>
-//         </div>
-//       </div>
-
-//                     {/* end slide three */}
-                    
-//                     {/* slide four */}
-//                     <div>
-//         <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-//           <Image src={slide4} loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          
-//         </a>
-
-//         <div className="flex flex-col">
-//           <span className="text-gray-200">JHON WICK</span>
-//           <a href="#" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Business Causual</a>
-//         </div>
-//       </div>
-//                     {/* end slide four */}
-                    
-//                     {/* slide five */}
-//                     <div>
-//         <a href="#" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
-//           <Image src={slide5} loading="lazy" alt="Photo by Austin Wade" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          
-//         </a>
-
-//         <div className="flex flex-col">
-//           <span className="text-gray-200">JHON WICK</span>
-//           <a href="#" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Business Causual</a>
-//         </div>
-//       </div>
-//                     {/* end slide five */}
-//                 </Slider>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default MovieUpComingCard;
-
 "use client";
-
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
-
-// import required modules
 import { FreeMode, Navigation } from "swiper/modules";
 import Image from "next/image";
 import Link from "next/link";
 
-// eslint-disable-next-line @next/next/no-async-client-component
 const MovieUpComingCard = async () => {
   const res = await fetch(
     "https://vibewabe-server.vercel.app/video?category=movie"
   );
   const drama = await res.json();
-  console.log(drama);
 
   return (
     <div className="text-white max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5 my-10">
       <div className="relative group">
         <Link href="#">
           <h3 className="text-xl font-semibold mb-6 group-hover:text-red-500">
-          Tranding Movie
+            Tranding Movie
           </h3>
           <button className="absolute top-0 left-32 hidden group-hover:inline-block px-4 py-2 text-white rounded-md transition-transform transform group-hover:translate-x-2 hover:underline">
             See All
@@ -188,7 +39,7 @@ const MovieUpComingCard = async () => {
           1024: { slidesPerView: 4 },
         }}
       >
-        {drama.slice(10,20).map((data) => (
+        {drama.slice(10, 20).map((data) => (
           <SwiperSlide key={data.id}>
             <Link href={`/video/${data.id}`}>
               <div className="relative group overflow-hidden">
@@ -213,9 +64,3 @@ const MovieUpComingCard = async () => {
 };
 
 export default MovieUpComingCard;
-
-
-
-
-
-
