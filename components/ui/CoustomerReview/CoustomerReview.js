91832fd0a8f1ff22efd43a46fac7f669ef6384a9
hossainnirob <shajadanirob@gmail.com>
2024-02-09 18:53:31 +0600

    change code

--- components/ui/CoustomerReview/CoustomerReview.js
+++ components/ui/CoustomerReview/CoustomerReview.js
@@ -27,15 +27,21 @@ const CoustomerReview = ({id}) => {
 
 
 
+const [comments, setComment] = useState([]);
+const [loading, setLoading] = useState(true);
 
-const [comments , setComment] = useState([])
-useEffect(() =>{
-  axios.get(`https://vibewabe-server.vercel.app/videoComment/individual/${id}`)
-  .then(res =>{
-   setComment(res.data)
-
-  })  
-},[id])
+useEffect(() => {
+  axios
+    .get(`https://vibewabe-server.vercel.app/videoComment/individual/${id}`)
+    .then((res) => {
+      setComment(res.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error fetching comments:", error);
+      setLoading(false);
+    });
+}, [id]);
 
 
 
