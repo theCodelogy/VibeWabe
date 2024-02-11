8fe9cd201c983688f1768961ea9c440a68a3bf58
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-02-11 22:10:41 +0600

    add new change on dropdown

--- components/shared/navbar/navComponents/NavBtn.jsx
+++ components/shared/navbar/navComponents/NavBtn.jsx
@@ -1,26 +1,129 @@
+// "use client";
+// import LanguageChanger from "@/components/LanguageChanger/LanguageChanger";
+// import { authContext } from "@/utils/AuthProvider";
+// import Link from "next/link";
+// import { useContext, useState } from "react";
+// import { useTranslation } from "react-i18next";
+
+// const NavBtn = () => {
+//   const { user, signout } = useContext(authContext);
+//   const [showProfile, setShowProfile] = useState(false);
+//   const { t } = useTranslation();
+//   return (
+//     <>
+//       {user?.email ? (
+//         <>
+//           <LanguageChanger />
+//           <div
+//             onClick={() => {
+//               setShowProfile(!showProfile);
+//             }}
+//             className="dropdown dropdown-end"
+//           >
+//             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+//               <div className="w-10 rounded-full">
+//                 <img
+//                   src={
+//                     user?.photoURL
+//                       ? user?.photoURL
+//                       : "https://i.ibb.co/vcQZhpc/1921098.png"
+//                   }
+//                 />
+//               </div>
+//             </label>
+//             {showProfile && (
+//               <ul
+//                 tabIndex={0}
+//                 className="mt-3 z-[1] p-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box space-y-2"
+//               >
+//                 <li>
+//                   <p>{user?.displayName}</p>
+//                 </li>
+//                 <li>
+//                   <p>{user?.email}</p>
+//                 </li>
+//                 <li>
+//                   <Link href={"/dashboard/profile"}>Dashboard</Link>
+//                 </li>
+//                 <li>
+//                   <Link href={"/mystuff"}>My Stuff</Link>
+//                 </li>
+//               </ul>
+//             )}
+//           </div>
+//           <button
+//             onClick={signout}
+//             className="bg-gradient-to-r from-red-600 to-orange-500 drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+//             // className="bg-[#F29F05] drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+//           >
+//             {t("Logout")}
+//           </button>
+//         </>
+//       ) : (
+//         <div className="flex sm:gap-5 lg:gap-8 uppercase">
+//           <LanguageChanger />
+//           <Link
+//             href={"/login"}
+//             className="bg-gradient-to-r from-red-600 to-orange-500 drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+//           >
+//             {t("login")}
+//           </Link>
+//           <Link
+//             href={"/signUp"}
+//             className="text-white border border-bg-gradient-to-r from-red-600 to-orange-500 px-4 py-2 rounded-full"
+//           >
+//             {t("signup")}
+//           </Link>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default NavBtn;
+
 "use client";
+import { useEffect, useRef } from "react";
 import LanguageChanger from "@/components/LanguageChanger/LanguageChanger";
 import { authContext } from "@/utils/AuthProvider";
 import Link from "next/link";
 import { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { IoLogOutOutline } from "react-icons/io5";
+import { AnimatePresence, motion } from "framer-motion";
 
 const NavBtn = () => {
   const { user, signout } = useContext(authContext);
   const [showProfile, setShowProfile] = useState(false);
   const { t } = useTranslation();
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowProfile(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       {user?.email ? (
         <>
           <LanguageChanger />
-          <div
-            onClick={() => {
-              setShowProfile(!showProfile);
-            }}
-            className="dropdown dropdown-end"
-          >
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div ref={dropdownRef} className="dropdown dropdown-end">
+            <label
+              onClick={() => {
+                setShowProfile(!showProfile);
+              }}
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar"
+            >
               <div className="w-10 rounded-full">
                 <img
                   src={
@@ -31,33 +134,60 @@ const NavBtn = () => {
                 />
               </div>
             </label>
-            {showProfile && (
-              <ul
-                tabIndex={0}
-                className="mt-3 z-[1] p-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box space-y-2"
-              >
-                <li>
-                  <p>{user?.displayName}</p>
-                </li>
-                <li>
-                  <p>{user?.email}</p>
-                </li>
-                <li>
-                  <Link href={"/dashboard/profile"}>Dashboard</Link>
-                </li>
-                <li>
-                  <Link href={"/mystuff"}>My Stuff</Link>
-                </li>
-              </ul>
-            )}
+            <AnimatePresence>
+              {showProfile && (
+                <motion.ul
+                  initial={{ scaleY: 0 }}
+                  animate={{
+                    scaleY: 1,
+                    transition: { duration: 0.2, ease: [0.12, 0, 0.39, 0] },
+                  }}
+                  exit={{
+                    scaleY: 0,
+                    transition: { duration: 0.2, ease: [0.22, 1, 0.36, 1] },
+                  }}
+                  tabIndex={0}
+                  className="mt-3 z-[1] p-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box space-y-2"
+                >
+                  <div className="flex gap-3">
+                    <div className="w-10 rounded-full overflow-hidden">
+                      <img
+                        className="w-full h-full"
+                        src={
+                          user?.photoURL
+                            ? user?.photoURL
+                            : "https://i.ibb.co/vcQZhpc/1921098.png"
+                        }
+                      />
+                    </div>
+                    <div>
+                      <p className="font-bold">{user?.displayName}</p>
+                      <p>{user?.email}</p>
+                    </div>
+                  </div>
+                  <div className="divider"></div>
+                  <li>
+                    <Link href={"/dashboard/profile"}>Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link href={"/mystuff"}>My Stuff</Link>
+                  </li>
+                  <li>
+                    <Link href={"/feedback"}>Feedback</Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={signout}
+                      className="bg-gradient-to-r from-red-600 to-orange-500 drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
+                    >
+                      <IoLogOutOutline className="text-2xl" />
+                      {t("Logout")}
+                    </button>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
           </div>
-          <button
-            onClick={signout}
-            className="bg-gradient-to-r from-red-600 to-orange-500 drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
-            // className="bg-[#F29F05] drop-shadow-md text-white transition-all hover:scale-95 ease-in-out duration-200 py-2 px-8 rounded-xl flex items-center justify-center"
-          >
-            {t("Logout")}
-          </button>
         </>
       ) : (
         <div className="flex sm:gap-5 lg:gap-8 uppercase">
--- components/ui/cards/Slider.js
+++ components/ui/cards/Slider.js
@@ -1,4 +1,103 @@
-'use client'
+// "use client";
+// import { useContext, useEffect, useState } from "react";
+// import { FreeMode, Navigation } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { useTranslation } from "react-i18next";
+// import { motion } from "framer-motion";
+// import "swiper/css/navigation";
+// import Image from "next/image";
+// import "swiper/css/free-mode";
+// import Link from "next/link";
+// import axios from "axios";
+// import "swiper/css";
+// import { authContext } from "@/utils/AuthProvider";
+
+// const Slider = ({ category, tags, title }) => {
+//   const { t } = useTranslation();
+//   const [videos, setVideos] = useState([]);
+//   const { user } = useContext(authContext);
+
+//   useEffect(() => {
+//     axios
+//       .get(
+//         `https://vibewabe-server.vercel.app/video?category=${category}&tags=${tags}`
+//       )
+//       .then((res) => {
+//         setVideos(res.data);
+//       });
+//   }, [category, tags]);
+
+//   const handleWatch = (video) => {
+//     const allData = {
+//       videoId: video._id,
+//       email: user.email,
+//       thambnail: video.thambnail,
+//       title: video.title,
+//     };
+//     axios
+//       .post("https://vibewabe-server.vercel.app/videoHistory", allData)
+//       .then((res) => console.log(res.data));
+//     console.log("User email:", user.email); // Add this console.log
+//     console.log("Video clicked:", allData);
+//   };
+
+//   return (
+//     <div className="text-white max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5 my-10">
+//       <motion.div className="py-3 group relative flex justify-start items-center">
+//         <h2 className="text-2xl font-semibold group-hover:text-red-500">
+//           {t(`${title}`)}
+//         </h2>
+//         <Link
+//           href={`video/${category}/${tags}`}
+//           className="hidden group-hover:inline-block px-2 py-1 text-white rounded-md transition-transform transform group-hover:translate-x-2 hover:underline"
+//         >
+//           See All
+//         </Link>
+//       </motion.div>
+//       <Swiper
+//         slidesPerView={1}
+//         spaceBetween={10}
+//         freeMode={true}
+//         loop={true}
+//         navigation={true}
+//         modules={[FreeMode, Navigation]}
+//         className="mySwiper"
+//         breakpoints={{
+//           640: { slidesPerView: 1 },
+//           768: { slidesPerView: 3 },
+//           1024: { slidesPerView: 5 },
+//         }}
+//       >
+//         {videos.map((video) => (
+//           <SwiperSlide key={video?._id}>
+//             <div
+//               onClick={() => handleWatch(video)}
+//               className="relative group overflow-hidden rounded-xl"
+//             >
+//               <Link href={`/video/${video?._id}`}>
+//                 <Image
+//                   className="w-[350px] md:w-[300px] h-[350px] transform transition-transform duration-300 group-hover:scale-110"
+//                   src={video?.thambnail}
+//                   width={350}
+//                   height={350}
+//                   alt={video?.title}
+//                   priority={true}
+//                 />
+//               </Link>
+//               <h5 className="text-center mt-1 text-lg font-semibold">
+//                 {video?.title}
+//               </h5>
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default Slider;
+
+"use client";
 import { useContext, useEffect, useState } from "react";
 import { FreeMode, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -11,6 +110,7 @@ import Link from "next/link";
 import axios from "axios";
 import "swiper/css";
 import { authContext } from "@/utils/AuthProvider";
+import useVideoFetch from "@/utils/hooks/useVideoFetch";
 
 const Slider = ({ category, tags, title }) => {
   const { t } = useTranslation();
@@ -18,22 +118,32 @@ const Slider = ({ category, tags, title }) => {
   const { user } = useContext(authContext);
 
   useEffect(() => {
-    axios.get(
-        `https://vibewabe-server.vercel.app/video?category=${category}&tags=${tags}`
-      )
-      .then((res) => {
-        setVideos(res.data);
-      });
+    const fetchData = async () => {
+      try {
+        const { data } = await useVideoFetch(category, tags);
+        setVideos(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, [category, tags]);
 
   const handleWatch = (video) => {
-    const allData = {videoId : video._id  , email: user.email ,thambnail: video.thambnail , title: video.title,}
-    axios.post('https://vibewabe-server.vercel.app/videoHistory',allData )
-    .then(res => console.log(res.data))
-    console.log("User email:", user.email); // Add this console.log
-    console.log("Video clicked:",allData);
-   
-  }
+    const allData = {
+      videoId: video._id,
+      email: user.email,
+      thambnail: video.thambnail,
+      title: video.title,
+    };
+    axios
+      .post("https://vibewabe-server.vercel.app/videoHistory", allData)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.error("Error posting data:", error));
+    console.log("User email:", user.email);
+    console.log("Video clicked:", allData);
+  };
 
   return (
     <div className="text-white max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5 my-10">
@@ -41,12 +151,14 @@ const Slider = ({ category, tags, title }) => {
         <h2 className="text-2xl font-semibold group-hover:text-red-500">
           {t(`${title}`)}
         </h2>
-        <Link
-          href={`video/${category}/${tags}`}
-          className="hidden group-hover:inline-block px-2 py-1 text-white rounded-md transition-transform transform group-hover:translate-x-2 hover:underline"
-        >
-          See All
-        </Link>
+        <motion.div>
+          <Link
+            href={`video/${category}/${tags}`}
+            className="hidden group-hover:inline-block px-2 py-1 text-white rounded-md transition-transform transform group-hover:translate-x-2 hover:underline"
+          >
+            See All
+          </Link>
+        </motion.div>
       </motion.div>
       <Swiper
         slidesPerView={1}
@@ -62,9 +174,12 @@ const Slider = ({ category, tags, title }) => {
           1024: { slidesPerView: 5 },
         }}
       >
-        {videos.map((video) => (
+        {videos?.map((video) => (
           <SwiperSlide key={video?._id}>
-            <div onClick={() => handleWatch(video)} className="relative group overflow-hidden rounded-xl">
+            <div
+              onClick={() => handleWatch(video)}
+              className="relative group overflow-hidden rounded-xl"
+            >
               <Link href={`/video/${video?._id}`}>
                 <Image
                   className="w-[350px] md:w-[300px] h-[350px] transform transition-transform duration-300 group-hover:scale-110"
