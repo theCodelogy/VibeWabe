47837870dbc8edcb52a5246659bbb45c67e2df95
tanishaislam <dev.eva.2006@gmail.com>
2024-02-21 16:45:54 +0600

    dynamic music category card

--- components/ui/categoryGroupComponents/MusicCategory/MusicSubCategory.js/AllMusicCard.js
+++ components/ui/categoryGroupComponents/MusicCategory/MusicSubCategory.js/AllMusicCard.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FaCirclePlay } from "react-icons/fa6";
-import musicCard from "@/assets/Musics/card8.jpg"
+import Link from "next/link";
 
 const AllMusicCard = ({AllmusicShows}) => {
     return (
@@ -10,12 +10,14 @@ const AllMusicCard = ({AllmusicShows}) => {
                     AllmusicShows.map(AllmusicShow=><>
                     <div key={AllmusicShow?._id} className="mb-20">
                         <div className='group relative rounded-lg lg:w-48 md:w-48 w-auto lg:h-48 md:h-48 h-56 mt-5 mx-5'>
+                        <Link href={`/music/${AllmusicShow?._id}`}>
                             <Image className='w-full h-full rounded-lg mb-2 transition duration-300' src={AllmusicShow?.thambnail} width={200} height={200} alt='slice image' />
 
                             <div className='absolute text-3xl text-white rounded-lg -top-0 left-0  flex justify-center items-center lg:w-48 md:w-48 w-full lg:h-48 md:h-48 h-56 bg-black bg-opacity-55 opacity-0 group-hover:opacity-100 transition-opacity duration-300 '>
                                 <FaCirclePlay className='cursor-pointer'></FaCirclePlay>
                             </div>
                             <h3 className='text-white hover:text-sky-400'>{AllmusicShow?.title}</h3>
+                           </Link>
                             <p className='text-sm text-zinc-500'>{AllmusicShow?.singerName}</p>
                         </div>
                         </div>
